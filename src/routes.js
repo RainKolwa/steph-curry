28a35de207771dd5de1b187dfb347e5bdbad2fe5
Lelouch Joshua <lelouchjoshua@gmail.com>
2017-06-28 11:28:04 +0800

    [+] profilePage init

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import { App } from './containers'
-import { DashboardPage, UserCenterPage, LoginPage } from './containers/'
+import { DashboardPage, UserCenterPage, LoginPage, ProfilePage } from './containers/'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={DashboardPage} />
     <Route path="login" component={LoginPage} />
     <Route path="my" component={UserCenterPage} />
+    <Route path="my/profile" component={ProfilePage} />
   </Route>
 )
