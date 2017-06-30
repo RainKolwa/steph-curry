6cc9c47af00fb506e62f408f21095833dadd4f95
rainkolwa <pluvo.lee@gmail.com>
2017-06-30 16:42:07 +0800

    [!] update to msg

--- src/middleware/launch.js
+++ src/middleware/launch.js
@@ -1,68 +1,68 @@
-const apiRoot = process.env.REACT_APP_API_ROOT;
+const apiRoot = process.env.REACT_APP_API_ROOT
 
 const callApi = (endpoint, data, method, headers) => {
   const fullUrl =
-    endpoint.indexOf(apiRoot) === -1 ? apiRoot + endpoint : endpoint;
+    endpoint.indexOf(apiRoot) === -1 ? apiRoot + endpoint : endpoint
 
   return fetch(fullUrl, {
     method,
     headers: headers || {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "Fs-Request-From": process.env.REACT_APP_REQUEST_FROM
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'Fs-Request-From': process.env.REACT_APP_REQUEST_FROM,
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   }).then(response =>
     response.json().then(json => {
       if (json.code !== 0) {
-        return Promise.reject(json);
+        return Promise.reject(json)
       }
 
-      return json;
+      return json
     })
-  );
-};
+  )
+}
 
-export const LAUNCH_API = "Call API2";
+export const LAUNCH_API = 'Call API2'
 
 export default store => next => action => {
-  const callAPI = action[LAUNCH_API];
-  if (typeof callAPI === "undefined") {
-    return next(action);
+  const callAPI = action[LAUNCH_API]
+  if (typeof callAPI === 'undefined') {
+    return next(action)
   }
 
-  let { endpoint } = callAPI;
-  const { types, data, method, headers } = callAPI;
+  let { endpoint } = callAPI
+  const { types, data, method, headers } = callAPI
 
-  if (typeof endpoint === "function") {
-    endpoint = endpoint(store.getState());
+  if (typeof endpoint === 'function') {
+    endpoint = endpoint(store.getState())
   }
 
   const actionWith = data => {
-    const finalAction = Object.assign({}, action, data);
-    delete finalAction[LAUNCH_API];
-    return finalAction;
-  };
+    const finalAction = Object.assign({}, action, data)
+    delete finalAction[LAUNCH_API]
+    return finalAction
+  }
 
-  const [requestType, successType, failureType] = types;
-  next(actionWith({ type: requestType }));
+  const [requestType, successType, failureType] = types
+  next(actionWith({ type: requestType }))
 
   return callApi(endpoint, data, method, headers).then(
     response =>
       next(
         actionWith({
           response,
-          type: successType
+          type: successType,
         })
       ),
     error => {
-      console.log("stack", error.stack);
+      console.log('stack', error.stack)
       return next(
         actionWith({
           type: failureType,
-          error: error.message || "Something bad happened"
+          error: error.msg || 'Something bad happened',
         })
-      );
+      )
     }
-  );
-};
+  )
+}
